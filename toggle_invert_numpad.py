ac15b37a65fa8af99e9998ebdd7bb4c6e8148c3f
Josh Glottmann <josh.glottmann@gmail.com>
2025-02-21 00:02:47 -0500

    Updated toggle_invert_numpad

--- toggle_invert_numpad.py
+++ toggle_invert_numpad.py
@@ -1,8 +1,11 @@
 import os
 import json
+import time
 
 profiles_dir = os.getenv('LOCALAPPDATA') + os.sep + 'CRC' + os.sep + 'Profiles'
 
+i = 0
+invert_state = None
 for filename in os.listdir(profiles_dir):
     if filename.endswith('.json'):
         file_path = os.path.join(profiles_dir, filename)
@@ -10,11 +13,25 @@ for filename in os.listdir(profiles_dir):
         with open(file_path, 'r', encoding='utf-8') as file:
             data = json.load(file)
         
-        if 'InvertNumericKeypad' in data:
+        if 'InvertNumericKeypad' not in data:
+            continue
+
+        if i == 0:
             if data['InvertNumericKeypad']:
-                data['InvertNumericKeypad'] = None
+                invert_state = None
             else:
-                data['InvertNumericKeypad'] = True
+                invert_state = True
+
+        data['InvertNumericKeypad'] = invert_state
+
+        i += 1
 
         with open(file_path, 'w', encoding='utf-8') as file:
             json.dump(data, file, indent=2)
+
+if invert_state == None:
+    print('Numpad not inverted!')
+else:
+    print('Numpad inverted!')
+
+time.sleep(1)
